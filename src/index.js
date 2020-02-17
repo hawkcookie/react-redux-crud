a3df4d011da11e2816216d3fb247d0f459b1d005
k-maekawa <qian41119122@gmail.com>
2020-02-17 21:19:25 +0900

    add evnents index

--- src/index.js
+++ src/index.js
@@ -4,21 +4,27 @@ import { createStore, applyMiddleware } from "redux"
 import {  Provider } from "react-redux"
 import './index.css';
 import EventsIndex from './components/events_index';
+import EventsNew from "./components/events_new"
 import * as serviceWorker from './serviceWorker';
 import reducer from "./reducers"
 import thunk from "redux-thunk"
+
+import { BrowserRouter, Switch, Route} from "react-router-dom"
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <EventsIndex />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={EventsIndex}>
+        <EventsIndex />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
   );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
